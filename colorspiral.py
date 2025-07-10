d698da3957898a3a50f40d1ce1b17ace4dc4f1a6
Ibrahim Khan <ik1416486@gmail.com>
2025-07-10 12:52:13 -0700

    I added extra functions

--- colorspiral.py
+++ colorspiral.py
@@ -1,7 +1,7 @@
 # colorspiral.py
 """A module for drawing colorful spirals of up to 6 sides"""
 import turtle
-def cspiral(sides=6, size=360, x=0, y=0):
+def cspiral(sides=12, size=360, x=0, y=0):
     """Draws a colorful spiral on a black background.
     Arguments:
     sides -- the number of sides in the spiral (default 6)
@@ -14,10 +14,17 @@ def cspiral(sides=6, size=360, x=0, y=0):
     t.setpos(x,y)
     t.pendown()
     turtle.bgcolor("black")
-    colors=["red", "yellow", "blue", "orange", "green", "purple"]
-    for n in range(size):
-        t.pencolor(colors[n%sides])
-        t.forward(n * 3/sides + n)
-        t.left(360/sides + 1)
-        t.width(n*sides/100)
-
+    colors=["red", "yellow", "blue", "orange", "green", "purple", "pink", "cyan", "magenta", "lime", "brown", "gray"]
+    for n in range(0, size, 1):
+        t.pencolor(colors[n % sides])
+        t.forward(n * 3 / sides + n)
+        t.left(360 / sides + 1)
+        t.width(n * sides / 100)
+        writeToTerminal(f"Drawing side {n} and the color this time is {colors[n % sides]}")
+        # writeToTerminal(f"Drawing side {n+1} with color {colors[n%sides]}")
+        # writeToTerminal(f"Drawing side with color ")
+        turtle.time.sleep(0.5)
+def writeToTerminal(msgIbi):
+    """Writes a message to the terminal"""
+    print(msgIbi)
+cspiral()
